7fea159ce296da9efef3ae75524d30843edc600d
DHIRAJ KUMAR <moneybirds111@gmail.com>
2023-02-16 00:48:54 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>BilboTech</h1>
+                <p>@all right reserved</p>
+            </div>
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href='https://www.youtube.com/@AllAboutArtOriginal' target={"blank"}>Youtube</a>
+                    <a href='https://www.instagram.com/dhirajkumarreo/' target={"blank"}>Instagram </a>
+                    <a href='https://github.com/dhiraj-source' target={"blank"}>Github</a>
+                </div>
+            </div>
+        </footer>)
+}
+
+export default Footer
